efed2934a22c134db889c9d0dadf3321d6cffc70
bcha92@hotmail.com <bcha92@hotmail.com>
2021-10-18 23:51:01 -0400

    --screenshots and modifications

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Python-HTML
 HTML webpage rendering using Python Flask
+
+<img src="screenshots/screen1.JPG" alt="raw page" />
+
+
+---
+
+### Perhaps I've been fooling around for too long.
+
+Time to move on, but feel free to play with this repository as you please if you are interested in furthering web development with Python. I'm just playing with some basic FLASK library modules here. :)
+
+You can also "send_static_file" to directly retrieve JSON files via Python Webpage
+
+<img src="screenshots/screen2.JPG" alt="raw code" />
--- app.py
+++ app.py
@@ -28,11 +28,34 @@ def about():
 def get_data():
     return app.send_static_file("data.json")
 
+# "/raw" RAW (renders html raw format)
 @app.route("/raw")
 def raw():
     return """
-    <div>
-        <h1>RAW PAGE</h1>
-        <p>Custom Generated Page for </p>
-    </div>
+    <!DOCTYPE HTML>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <title>RAW</title>
+            <link rel="stylesheet" type="text/css" href="/static/styles.css">
+        </head>
+        <body>
+            <header class="flex">
+                <a href="/" class="nav title">FLASK</a>
+                <a href="/" class="nav">Home</a>
+                <a href="/about" class="nav">About</a>
+                <a href="/api/data" class="nav">Data</a>
+                <a href="/raw" class="nav">RAW</a>
+            </header>
+            <div class="root flex">
+                <h2>RAW PAGE</h2>
+                <p>Custom Generated HTML Page for "/raw" extension using "app.py" only</p>
+                <h3>See the Difference</h3>
+                <p class="believe">
+                    <span class="see">Anything is possible!</span> Just unleash your creativity and go with it!
+                </p>
+                <p>Sometimes, it only takes a bit of courage to try something new!</p>
+            </div>
+        </body>
+    </html>
     """
