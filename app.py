716e283959b5ea9ace4765e7bc72d09ec7dce30b
bcha92@hotmail.com <bcha92@hotmail.com>
2021-10-18 23:28:57 -0400

    --customization

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
 from datetime import datetime
-import re
 
 # Main Flask App
 app = Flask(__name__)
@@ -8,13 +7,20 @@ app = Flask(__name__)
 # Homepage "/"
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("extend.html",
+    title="FLASK HOME",
+    subtitle="Home Page",
+    content="This site was generated using VSCode Python FLASK Library"
+    )
 
-# "/hello/<name>" Route
-@app.route("/hello/<name>")
-def hello_there(name = None):
-    return render_template(
-        "index.html", name=name, date=datetime.now()
+# "/about/" Route
+@app.route("/about")
+def about():
+    return render_template("extend.html",
+    title="FLASK ABOUT",
+    subtitle="About",
+    content="Some neat tricks with Flask",
+    subcontent="Date and time right now: " + datetime.now().strftime("%A, %d %B, %Y at %X")
     )
 
 # "/api/data" Route (renders json file)
@@ -22,7 +28,11 @@ def hello_there(name = None):
 def get_data():
     return app.send_static_file("data.json")
 
-# "/about/" Route
-@app.route("/about/")
-def about():
-    return render_template("about.html")
+@app.route("/raw")
+def raw():
+    return """
+    <div>
+        <h1>RAW PAGE</h1>
+        <p>Custom Generated Page for </p>
+    </div>
+    """
